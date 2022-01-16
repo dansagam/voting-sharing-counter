c1c507f27d9e7134d6eb75e88c3a5528b176159b
dansagam <olukayodeogunnowo@yahoo.com>
2022-01-16 21:01:24 +0100

    setting up the routes

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const path = require('path')
+import express from 'express'
+import path from 'path'
 const PORT = process.env.PORT || 5000
+const app = express()
 
+const __dirname = path.resolve()
 express()
+  .use(express.json())
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
